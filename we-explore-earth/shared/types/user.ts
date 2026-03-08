af8ad5f1478be69c041a55b17d6056cebebdc6c5
Your Name <sanjay.krishnas@gmail.com>
2026-03-08 16:15:40 -0700

    Consolidated user.ts in shared folder for onboarding and avatars

--- we-explore-earth/shared/types/user.ts
+++ we-explore-earth/shared/types/user.ts
@@ -14,25 +14,6 @@ export interface NewUser {
   isAdmin: boolean;
   events: UserRSVP[];
   avatar: string | null;
-}
-
-export interface User extends NewUser {
-  id: string; 
-import { RSVPStatus } from './event';
-
-export interface UserRSVP {
-  eventID: string;
-  status: RSVPStatus;
-}
-
-export interface NewUser {
-  username: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  notificationToken: string | null;
-  isAdmin: boolean;
-  events: UserRSVP[];
   hasOnboarded: boolean;
 }
 
