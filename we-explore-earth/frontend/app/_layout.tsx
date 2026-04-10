d5ddcb0c052edf6eadf546e472d2984a360b36d5
Vanshika Turkar <vanshika.turkar@gmail.com>
2026-04-09 22:16:10 -0700

    Added Typography File (Fonts)

--- we-explore-earth/frontend/app/_layout.tsx
+++ we-explore-earth/frontend/app/_layout.tsx
@@ -5,8 +5,31 @@ import { Stack } from 'expo-router';
 import { Provider } from 'react-redux';
 //LOCAL FILES
 import { store } from './redux/store';
+import * as SplashScreen from 'expo-splash-screen';
+import {useFonts} from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'LibreBaskerville-Regular': require('../../shared/fonts/LibreBaskerville-Regular.ttf'),
+    'HankenGrotesk-Regular': require('../../shared/fonts/HankenGrotesk-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
  return (
     <Provider store={store}>
       <Stack 
