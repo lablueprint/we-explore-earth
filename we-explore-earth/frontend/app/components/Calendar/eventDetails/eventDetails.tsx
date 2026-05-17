ae7fb1db25aefc2d2e00d33b941e05a64b56dbe6
Vanshika Turkar <vanshika.turkar@gmail.com>
2026-05-17 16:35:39 -0700

    Revert "added the admin check to not be able to rsvp"
    This reverts commit dd3fb549c3039afc5c96004641d7e93079aeef04.

--- we-explore-earth/frontend/app/components/Calendar/eventDetails/eventDetails.tsx
+++ we-explore-earth/frontend/app/components/Calendar/eventDetails/eventDetails.tsx
@@ -7,6 +7,7 @@ import {
   Alert,
   ScrollView,
   StyleSheet,
+  Image
 } from "react-native";
 import { useRouter, useSegments, useFocusEffect } from "expo-router";
 
@@ -154,6 +155,11 @@ export default function EventDetails({
     }, [currentRSVP])
   );
 
+  const formatPrice = (price?: number | null) => {
+    if (price == null || price === 0) return "Free";
+    return `${price}`;
+  };
+
   if (!event) return null;
 
   return (
@@ -210,7 +216,10 @@ export default function EventDetails({
               </View>
 
               <View style={styles.infoRow}>
-                <Text style={styles.infoIcon}>🗓️</Text>
+                <Image
+                      source={require("../../../../../shared/images/calendar.png")}
+                      style={styles.logisticsIcon}
+                    />
                 <View>
                   <Text style={styles.infoTitle}>
                   {formatEventDateLine(event.timeStart, event.timeEnd)}
@@ -222,14 +231,16 @@ export default function EventDetails({
               </View>
 
               <View style={styles.infoRow}>
-                <Text style={styles.infoIcon}>📍</Text>
+                <Image
+                      source={require("../../../../../shared/images/location.png")}
+                      style={styles.logisticsIcon}
+                    />
                 <View>
                   <Text style={styles.infoTitle}>{event.location}</Text>
                 </View>
               </View>
 
               <Text style={styles.sectionLabel}>ATTENDEES</Text>
-
               <View style={styles.attendeeHeader}>
                 <Text style={styles.attendeeCount}>{event.attendees?.length ?? 0} People on the List</Text>
 
@@ -257,12 +268,13 @@ export default function EventDetails({
                
               </View>
 
-              {!isAdmin && (
-                <TouchableOpacity
-                  onPress={handleRSVPPress}
-                  style={styles.rsvpButton}
-                >
-                  
+
+              <TouchableOpacity
+                onPress={handleRSVPPress}
+                style={styles.rsvpButton}
+              >
+
+                
                 <Text style={styles.rsvpButtonText}>
                   {displayRSVP ? "Update RSVP" : "RSVP"}
                 </Text>
@@ -284,6 +296,51 @@ export default function EventDetails({
               <Text style={styles.body}>
                 {event.description || "No description provided."}
               </Text>
+
+              <Text style={styles.sectionLabel}>HOST</Text>
+
+              <View style={styles.infoCard}>
+                <Text style={styles.infoCardText}>
+                  {event.hostedBy}
+                </Text>
+              </View>
+
+
+              <Text style={styles.sectionLabel}>LOGISTICS</Text>
+              <View style={styles.infoCard}>
+                <Text style={styles.infoCardLabel}>
+                  Maximum capacity
+                </Text>
+
+                <View style={styles.logisticsRow}>
+                  <Image
+                    source={require("../../../../../shared/images/clock_loader_40.png")}
+                    style={styles.logisticsIcon}
+                  />
+
+                  <Text style={styles.infoCardText}>
+                    {event.maxAttendees} {event.maxAttendees === 1 ? "person" : "people"}
+                  </Text>
+                </View>
+              </View>
+
+              <View style={styles.infoCard}>
+                <Text style={styles.infoCardLabel}>
+                  Price per person
+                </Text>
+
+                <View style={styles.logisticsRow}>
+                  <Image
+                    source={require("../../../../../shared/images/attach_money.png")}
+                    style={styles.logisticsIcon}
+                  />
+
+                  <Text style={styles.infoCardText}>
+                    {formatPrice(event.price)} 
+                  </Text>
+                </View>
+              </View>
+
 
             </ScrollView>
           </View>
